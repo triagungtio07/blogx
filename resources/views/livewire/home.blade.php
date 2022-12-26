9700fa97c48b9f7e75d59a87922866199301a592
triagungtio <agung.prasetio@rebelworks.co>
2022-12-26 14:02:29 +0700

    change env naming

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -21,7 +21,7 @@
             </div>
         @endif
         <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-            Welcome to The BlogX
+            Welcome to The BlogX - Production Environtment
         </h1>
     </div>
 
