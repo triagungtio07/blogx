b25b7e6f4d3d1421be3ca4999d96d95fe9f0588e
triagungtio <agung.prasetio@rebelworks.co>
2022-12-28 20:16:35 +0700

    update title

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -21,7 +21,7 @@
             </div>
         @endif
         <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-            Welcome to The BlogX - Dev Environtment
+            Welcome to The BlogX - Dev Environtment v2
         </h1>
     </div>
 
