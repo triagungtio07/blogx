31609400ca704f55bf5a8c0a18b6fd56ca0cd8af
triagungtio <agung.prasetio@rebelworks.co>
2022-12-28 20:25:19 +0700

    update title prod

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -21,7 +21,7 @@
             </div>
         @endif
         <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-            Welcome to The BlogX - Production Environtment v2
+            Welcome to The BlogX - Production Environtment v3
         </h1>
     </div>
 
